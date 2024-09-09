99107a0276d0ea39ffef09b555f19d461e16a8bc
Cody Sauermann <codysauermann@gmail.com>
2024-09-09 15:27:41 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System.Text;
+using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,2 @@
+# Asp.Net-Angular-Tutorial
+ASP.Net and Angular application from course: https://www.udemy.com/course/build-an-app-with-aspnet-core-and-angular-from-scratch/
